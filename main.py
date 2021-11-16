666db0cee908d5d458d1bce2b77b0a6fbacbe6cd
Victor Phillips <vmattphillips@gmail.com>
2021-11-15 23:43:01 -0500

    file downloads dir changed

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ youtube_dl.utils.bug_reports_message = lambda: ''
 
 ytdl_format_options = {
     'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'outtmpl': './downloads/%(extractor)s-%(id)s-%(title)s.%(ext)s',
     'restrictfilenames': True,
     'noplaylist': True,
     'nocheckcertificate': True,
